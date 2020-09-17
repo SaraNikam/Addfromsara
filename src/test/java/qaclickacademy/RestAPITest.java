362433767ce1577a1be3b868802f4528ce793afc
nikamk <sarasbhat@gmail.com>
2020-09-17 15:13:04 -0500

    first commit

---
+++ src/test/java/qaclickacademy/RestAPITest.java
@@ -0,0 +1,19 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void Postjira()
+	{
+		System.out.println("Postjira");
+	}
+	
+	@Test
+	public void DeleteTwitter()
+	{
+		System.out.println("DeleteTwitter");
+	}		
+
+}
